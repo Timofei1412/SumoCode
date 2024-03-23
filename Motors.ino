6d91c3b705cb21ce6b9f697b69226b1e1257ed31
Timofey Fedorov <ta14122008@gmail.com>
2024-03-23 16:51:29 +0300

    motorSetup

---
+++ Motors.ino
@@ -0,0 +1,12 @@
+#define M1A 5
+#define M1B 6
+#define M2A 7
+#define M2B 8
+
+
+void motorSetup(){
+    pinMode(M1A, OUTPUT);
+    pinMode(M1B, OUTPUT);
+    pinMode(M2A, OUTPUT);
+    pinMode(M2B, OUTPUT);
+}
